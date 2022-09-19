c465a01e0225d0bfb37fc4d82bc1852532b41fe6
Noureen Naaz <96674191+noureennaaz@users.noreply.github.com>
2022-09-20 00:09:29 +0530

    Add files via upload

---
+++ basics/Stack/stackbegin.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+int s,top=-1, a[20];
+
+int isempty()
+{
+    if(top==-1)
+        return 1 ;
+    else
+        return 0;
+}
+
+int isfull()
+{
+    if(top<s)
+       return 0;
+    
+    else
+       return 1;
+
+}
+
+void push()
+{
+    if(isfull()!=1){
+        //int n;
+        //printf("\nEnter Element :\n");
+        scanf("%d",&a[++top]);
+        //a[++top]=n;
+    }
+    else
+       printf("Overflow !\n");
+}
+
+void pop(){
+    if(isempty()==0)
+        printf("\n%d removed ",a[top--]);
+        
+    else
+    printf("\nAlready Empty !");
+
+}
+void display(){
+    printf("\nDisplaying:\n");
+    for(int i=top ; i>=0 ; i--){
+        printf("%d ",a[i]);
+    }
+}
+int main(){
+
+    printf("Enter 'number of  elements' to insert : ");
+    scanf("%d",&s);
+   printf("Enter %d elements :\n",s);
+    for(int i=0 ; i<s ;i++){
+        push();
+    }
+    
+
+    display();
+
+    pop();
+
+    display();
+    pop();
+    pop();
+    display();
+
+}
