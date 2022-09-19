a8115f9a958ccf42a7415f2e9be14946fcd3ba1e
Noureen Naaz <96674191+noureennaaz@users.noreply.github.com>
2022-09-20 01:17:35 +0530

    Create InsertAnElement.c

---
+++ basics/Arrays/InsertAnElement.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[11]={1,2,3,4,5,6,7,8,9,10};
+
+    int k,index ;
+
+    printf("Enter the element to insert :");
+    scanf("%d",&k);
+
+    printf("At index :");
+    scanf("%d",&index);
+
+    for(int i=10; i>index-1;i--){
+
+        arr[i]=arr[i-1];
+    }
+
+    arr[index]=k;
+    
+    for(int i=0 ; i<11 ; i++){
+        printf("%d ",arr[i]);
+    }
+
+}
