36f3261cd06fc606aaa757f91569214599fa4f03
Noureen Naaz <96674191+noureennaaz@users.noreply.github.com>
2023-01-05 15:40:36 +0530

    Update QuickSort.cpp

--- sorting/QuickSort.cpp
+++ sorting/QuickSort.cpp
@@ -1,52 +1,44 @@
 #include<iostream>
 using namespace std;
-
-//returns the correct valueof pivot 
-int partition(int arr[],int l, int h)
+int partition(int *arr , int low , int high)
 {
-    int j=l ,i=j-1,pivot=h,temp;
-
-    for( ; j < h ; j++)
+    int index=low;
+    for(int i=low; i<high ;i++)
     {
-        if(arr[j]<arr[pivot])
+        if(arr[i]<arr[high])
         {
-            temp=arr[j];
-            arr[j]=arr[++i];
-            arr[i]=temp;
+            swap(arr[index++],arr[i]);
         }
     }
-    temp=arr[j];
-    arr[j]=arr[++i];
-    arr[i]=temp;
-    return i;
-}
+    swap(arr[index], arr[high]);
+    return index;
 
-void QS(int arr[] ,int low ,int high)
-{
-    int k;
-    if(high>low)
+}
+void Quicksort(int *arr, int low , int high)
+{ 
+    if(low<high)
     {
-        k=partition(arr,low,high);
-        QS(arr,k+1,high);
-        QS(arr,low,k-1);
+        int pivot=partition(arr,low  , high);
+        Quicksort(arr, low , pivot-1);
+        Quicksort(arr, pivot+1, high); 
     }
-}
 
+}
 int main()
 {
-    int n;
-    cout<<"Enter size :";
+    int n, k;
+    cout<<"Enter size :"<<endl;
     cin>>n;
+
     int arr[n];
     for(int i=0 ; i<n ;i++)
     {
         cin>>arr[i];
-
+        
     }
-    QS(arr,0,n);
-    cout<<"Final Array: "<<endl;
-    for(int i=0 ;i<n ;i++){
+    Quicksort(arr,0, n-1);
+    for(int i=0 ; i<n ;i++)
+    {
         cout<<arr[i]<<" ";
     }
-
 }
